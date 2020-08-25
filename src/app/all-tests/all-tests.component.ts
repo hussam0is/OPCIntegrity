25833eb5647bcece4834ca24f390bb0fe37291e7
Shihirban <64308221+Shihirban@users.noreply.github.com>
2020-08-25 12:44:06 +0300

    search function

--- src/app/all-tests/all-tests.component.ts
+++ src/app/all-tests/all-tests.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import * as $ from 'jquery/dist/jquery.min.js';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-all-tests',
@@ -7,10 +10,59 @@ import {Router} from '@angular/router';
   styleUrls: ['./all-tests.component.scss']
 })
 export class AllTestsComponent implements OnInit {
+  testId: String; 
+  Group: String;
+  testTitle: string;
+  SuccessRates: string;
+  category: string;
+  testCase: string;
+  testSummary: string;
+  expectedResult: string;
+  notes: string;
+    list: object;
+    list_len: number;
+    search_list: object;
+    chunk_id: number;
 
-  constructor() { }
+
+  
+    constructor(private http: HttpClient) {}
+
 
   ngOnInit(): void {
   }
+  searchBuildby(){
+    var field = $("#field").val();
+    var term = $("#txt1").val();
+    this.http.get('http://localhost:5000//test?field='+ field + '&search_text=' + term).toPromise()
+    .then(res =>  {this.list = res; console.log(this.list); console.log(Object.keys(this.list).length); this.list_len = Object.keys(this.list).length;
+    var table = document.getElementById("allTestTable");
+    var table_titles = ["test_id", "test_case", "test_title", "category", "test_summary", "test_rates","group","notes"];
+    
+    //split to chunks of 10
+    var values = Object.values(this.list);
+    var counter = 0;
+    var portion = {};
+    var final = [];
+    for (var key in this.list) {
+      if (counter !== 0 && counter % 10 === 0) {
+        final.push(portion);
+        portion = {};
+      }
+      portion[key] = values[counter];
+      counter++;
+    }
+    final.push(portion);
+    this.search_list = final;
+    this.chunk_id = 0;
 
-}
+    //add first chunk to table
+    for (var i = 0; i < this.list_len && i < 10; i++) {
+      var d = table.getElementsByTagName("tr")[i+1];
+      for (var j = 0; j < 6; j++){
+        d.getElementsByTagName("td")[j].innerHTML = this.list[i][(table_titles[j])]; 
+     }
+    }
+  });
+  }
+  }
