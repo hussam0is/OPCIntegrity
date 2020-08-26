98573ee3a22b56dc0c4c8124371be6a750e382a1
Shihirban <64308221+Shihirban@users.noreply.github.com>
2020-08-26 22:43:19 +0300

    updated{all test + run build}

--- src/app/build-over-view/build-over-view.component.ts
+++ src/app/build-over-view/build-over-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import * as $ from 'jquery/dist/jquery.min.js';
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-build-over-view',
@@ -6,12 +9,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./build-over-view.component.scss']
 })
 export class BuildOverViewComponent implements OnInit {
+  testId: String; 
+  Group: String;
+  testTitle: string;
+  category: string;
+  testCase: string;
+  testSummary: string;
+  expectedResult: string;
+  list: object;
+  list_len: number;
+  search_list: object;
+  chunk_id: number;
+  displayList: Array<any>=[];
+  envList: object;
+
+
   
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+
+    this.http.get('http://localhost:5000/environment').toPromise().then(s=> {
+      this.envList = s;
+    for(var i=0; i<Object.keys(s).length; i++){
+      var select = document.getElementById("select");
+      var option = document.createElement('option');
+      option.text = option.value = s[i];
+      select.appendChild(option);
+      (<HTMLInputElement>document.getElementById("env_name")).value  = s[i];
+    }
+    });
   }
+
+
+  searchReportTo(){
+    this.displayList=[]
+    var field = $("#field").val();
+    var term = $("#txt1").val();
+    this.http.get('http://localhost:5000//user?field='+ field + '&search_text=' + term).toPromise()
+    .then(res =>  {this.list = res; this.list_len = Object.keys(this.list).length;
+
+    for (var i = 0;  i < this.list_len; i++){
+      this.displayList.push(this.list[i]);
+    }
+  });
+  }
+  
+
+
+
+
   runBuild(){
 
   }
